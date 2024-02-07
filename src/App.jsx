c1c2babdfc25b2d9b3868b8b3bca29f3744cf85a
Saqib Ali <saqib.ns111@gmail.com>
2024-02-08 00:19:42 +0500

    add netflix logo and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import Header from "./Components/Header/Header";
+import "./App.scss";
 
 function App() {
   return (
